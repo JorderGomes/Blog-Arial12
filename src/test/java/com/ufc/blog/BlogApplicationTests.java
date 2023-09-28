60ef0563a5004441d7ec98f23b7b8b927b21d279
JorderGomes <jordernoveau@gmail.com>
2023-09-28 09:49:35 -0300

    update files maven

---
+++ src/test/java/com/ufc/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufc.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
