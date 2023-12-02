26e12333f4dd4dc18fb1ddfab7cf60de06cdcc2e
JorderGomes <jordernoveau@gmail.com>
2023-12-02 16:21:50 -0300

    update mongo config

---
+++ backend_persistencia/src/main/java/br/ufc/quixada/blog/dao/mongo/PostDaoMongo.java
@@ -0,0 +1,12 @@
+package br.ufc.quixada.blog.dao.mongo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import br.ufc.quixada.blog.dao.PostDAO;
+import br.ufc.quixada.blog.models.Post;
+
+@Repository
+public interface PostDaoMongo extends PostDAO, MongoRepository<Post, String> {
+    
+}
